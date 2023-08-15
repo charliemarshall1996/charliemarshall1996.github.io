ff71a02dc7099a802c156b8d8e2dbf39d9ab8819
Charlie Marshall <charlie.marshall1996@gmail.com>
2023-08-15 23:23:24 +0100

    Moved to root dir

--- script/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", function(event) {
-    const dropdowns = document.querySelectorAll(".dropdown");
-    dropdowns.forEach(function(dropdown) {
-        if (!dropdown.contains(event.target)) {
-            const dropdownContent = dropdown.querySelector(".dropdown-content");
-            dropdownContent.style.display = "none";
-        }
-    });
-});
